d12ea3f530d78a69ad418f9766e1b03e2bcbba8e
Chrisz236 <44371223+Chrisz236@users.noreply.github.com>
2022-07-18 16:47:58 -0700

    Add detailed comment to Message class

--- Server/Message.java
+++ Server/Message.java
@@ -1,13 +1,12 @@
+/*
+  Author: Haolin Zhang
+  File:   Message.java
+  Date:   July 11, 2022
+  Ver:    1.0
 
-/**
- * Author: Haolin Zhang
- * File:   Message.java
- * Date:   July 11, 2022
- * Ver:    1.0
- * 
- * Description:
- *         Message class for sending messages through the network
- *         via Java socket connection.
+  Description:
+          Message class for sending messages through the network
+          via Java socket connection.
  */
 
 import java.io.Serializable;
@@ -15,7 +14,7 @@ import java.io.Serializable;
 public class Message implements Serializable {
     private String data;
 
-    /**
+    /*
      * default constructor for Message
      * @param: null
      * @return: null
@@ -24,14 +23,26 @@ public class Message implements Serializable {
         this.data = "[UNDEFINED]";
     }
 
+    /*
+     * constructor for Message with parameter
+     * @param msg
+     */
     public Message(String msg) {
         this.data = msg;
     }
 
+    /*
+     * member function to get data from Message object
+     * @return String
+     */
     public String getData() {
         return this.data;
     }
 
+    /*
+     * member function to set content of the Message object
+     * @param msg
+     */
     public void setData(String msg) {
         this.data = msg;
     }
