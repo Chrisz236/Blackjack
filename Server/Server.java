a32102ce034c533f4e24ef46f515260b0d606eb9
Chrisz236 <44371223+Chrisz236@users.noreply.github.com>
2022-07-19 15:42:54 -0700

    Finish validateLogin function

--- Server/Server.java
+++ Server/Server.java
@@ -1,17 +1,22 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class Server implements Runnable {
 
     String hostname;
     int port;
+    private static String fileName = "Server/userFile.txt";
+    Map<String, Account> clientInfo = new HashMap<>();
+    // LobbyManager lobbyManager = new LobbyManager();
 
-    boolean online = true;
+    boolean online = false;
 
     public Server(String host, int port) {
         this.hostname = host;
         this.port = port;
         System.out.println(this.toString());
+        loadUserData();
     }
 
     /*
@@ -21,24 +26,40 @@ public class Server implements Runnable {
      *                 false for invalid
      */
     public boolean validateLogin(Message msg) {
-        String[] loginMsg = msg.getData().split(",");
-        String username = loginMsg[0];
-        String password = loginMsg[1];
+        if (msg.getType().equals("login")) {
+            String info = msg.getData();
+            String[] line = info.split(",");
+            String username = line[0];
+            if (clientInfo.get(username).getPassword(username).equals(line[1])) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-        try (BufferedReader br = new BufferedReader(new FileReader("userFile.txt"))) {
+    public void loadUserData() {
+        // localData[0] = USERNAME
+        // localData[1] = PASSWORD
+        // localData[2] = BALANCE
+        try {
+            File file = new File(fileName);
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
             String textLine;
             while ((textLine = br.readLine()) != null) {
                 String[] localData = textLine.split(",");
-                if (username.equals(localData[0]) && password.equals(localData[1])) {
-                    return true;
-                }
+                Account tmpAcc = new Account(localData[0], localData[1],
+                        Integer.parseInt(localData[2].trim()));
+                clientInfo.put(localData[0], tmpAcc);
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("[userFile.txt not found]");
+            fr.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        return false;
+    }
+
+    public int getPlayerBalance(String username) {
+        return clientInfo.get(username).getBalance(username);
     }
 
     public void newConnections() {
@@ -46,7 +67,7 @@ public class Server implements Runnable {
     }
 
     public String toString() {
-        return String.format("Server is running on %s @port %d", hostname, port);
+        return String.format("Server is running on: \'%s\', at port \'%d\'", hostname, port);
     }
 
     @Override
