c870b9908370ffe7e30ee3d6de0ad3c365015f1a
Chrisz236 <44371223+Chrisz236@users.noreply.github.com>
2022-07-19 17:24:32 -0700

    Add comments for Account class

--- Server/Account.java
+++ Server/Account.java
@@ -12,20 +12,35 @@ public class Account {
     private String password;
     private int balance;
 
+    /*
+     * constructor for Account class, initialize all variables
+     */
     public Account(String username, String password, int balance) {
         this.username = username;
         this.password = password;
         this.balance = balance;
     }
 
+    /*
+     * getter function for password
+     * @return String password
+     */
     public String getPassword(String username) {
         return this.password;
     }
 
+    /*
+     * getter function for password
+     * @return int balance
+     */
     public int getBalance(String username) {
         return this.balance;
     }
 
+    /*
+     * setter function for balance
+     * @param int newBalance
+     */
     public void setBalance(int newBalance) {
         this.balance = newBalance;
     }
