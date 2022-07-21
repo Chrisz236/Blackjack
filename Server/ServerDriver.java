bb4656b0dd3436f9305d4bd2dc09c939bf87425e
Chrisz236 <44371223+Chrisz236@users.noreply.github.com>
2022-07-21 14:02:05 -0700

    Remove useless test code in ServerDriver.java

--- Server/ServerDriver.java
+++ Server/ServerDriver.java
@@ -13,25 +13,6 @@ public class ServerDriver {
 
     public static void main(String args[]) {
         Server server = new Server(host, port);
-
-        // ------------------- FOR TEST PURPOSE ONLY -------------------- //
-//        Message m = new Message("HAOLIN ZHANG, hZhang", "login");
-//
-//        if(server.validateLogin(m)) {
-//            System.out.println("login verified!");
-//        } else {
-//            System.out.println("login failed!");
-//        }
-//
-//        System.out.print("Balance for HAOLIN ZHANG: ");
-//        System.out.println(server.getPlayerBalance("HAOLIN ZHANG"));
-//
-//        System.out.print("\nChanging balance to 8120...");
-//        server.setPlayerBalance("HAOLIN ZHANG", 8120);
-//        System.out.print("Balance for HAOLIN ZHANG: ");
-//        System.out.println(server.getPlayerBalance("HAOLIN ZHANG"));
-        // ------------------- FOR TEST PURPOSE ONLY -------------------- //
-
         server.run();
     }
 }
