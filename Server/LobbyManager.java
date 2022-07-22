fc44b4b46aefb3c4c7ecf5b9c47d0a2856de7699
Chrisz236 <44371223+Chrisz236@users.noreply.github.com>
2022-07-21 17:11:03 -0700

    Add displayLobbies, createLobby, deleteLobby functions

--- Server/LobbyManager.java
+++ Server/LobbyManager.java
@@ -1,5 +1,51 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class LobbyManager {
     private ArrayList<Lobby> lobbies;
+    private Map<String, Integer> lobbyIndex;
+    public int numOfLobbies;
+
+    public LobbyManager() {
+        lobbies = new ArrayList<>();
+        lobbyIndex = new HashMap<>();
+        numOfLobbies = 0;
+    }
+
+    public String displayLobbies() {
+        String lobbiesName = "";
+        if(lobbies.size() > 0) {
+            for(int i = 0; i < lobbies.size(); i++) {
+                lobbiesName += lobbies.get(i).getLobbyName() + "\n";
+            }
+        } else {
+            return "[NO LOBBY]";
+        }
+        return lobbiesName;
+    }
+
+    public LobbyStatus getLobbyStatus(String lobbyName) {
+        return lobbies.get(lobbyIndex.get(lobbyName)).getLobbyStatus();
+    }
+    public void createLobby(String lobbyName) {
+        Lobby lobby = new Lobby(lobbyName);
+        lobbies.add(lobby);
+        numOfLobbies++;
+    }
+
+    public boolean deleteLobby(String lobbyName) {
+        if(numOfLobbies == 0)
+            return false;
+        for(int i = 0 ; i < lobbies.size(); i++) {
+            if(lobbies.get(i).getLobbyName().equals(lobbyName)){
+                lobbies.remove(i);
+                numOfLobbies--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addPlayerToLobby(Player player, String lobbyName) {
+
+    }
 }
