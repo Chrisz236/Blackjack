316a5b6df630d2ee8d615aebaabb4e07a1fdea6b
dureno <98059551+dureno@users.noreply.github.com>
2022-07-22 22:17:52 -0700

    Create LoginInterface.java
    Creating LoginWindow folder.

---
+++ LoginWindow/LoginInterface.java
@@ -0,0 +1,16 @@
+/*
+ * Author: 		Diego Ureno
+ * File: 		LoginInterface.java
+ * 
+ * File Description: An interface for the LoginWindow
+ * class.
+ * 
+ */
+
+//Interface command for LoginWindow.
+public interface LoginInterface {
+	
+	//Process GUI functions
+	void processCommands();
+	
+}
