a8047e030af8d47e7492ea1d1f485e0065a2f933
Chrisz236 <44371223+Chrisz236@users.noreply.github.com>
2022-07-25 15:48:33 -0700

    Add comments

--- Server/server/DummyClient1.java
+++ Server/server/DummyClient1.java
@@ -20,6 +20,27 @@ public class DummyClient1 {
                 // Example of Login
                 oos.writeObject(new Message("HAOLIN ZHANG, hZhang", Type.Login));
 
+                /*
+                 * PLEASE READ:
+                 * In actual client, you will just receive the response from Server (a Message object)
+                 * but don't know what that message contains, so you will need a big switch to sort
+                 * all those message by "getType()" of that message, then fetch (decode) the "data"
+                 * by standard. For most of the cases, returned info are String and separated by comma ",".
+                 * The only one exception is:
+                 *              "Type.ShowAllPlayerInfo"
+                 *
+                 * The message with this return type, data part is a "Map<String, Player>" object, which
+                 * contains all players' info in Server, including:
+                 *              "username", (String)
+                 *              "password", (String)
+                 *              "balance",  (int)
+                 *              "isDealer"  (boolean)
+                 *              "oos"       (DO NOT MODIFY!)
+                 *
+                 * Then you can use this information to initialize your local player data for game play
+                 * More detailed usage can be found in code below.
+                 */
+
                 // Read message returned from Server
                 Message LoginResult = (Message) ois.readObject();
 
@@ -43,13 +64,15 @@ public class DummyClient1 {
                         // First type cast (get raw message)
                         Message msg1 = (Message) ois.readObject();
 
-                        // Second type cast (get all player's info as a Map)
-                        Map<String, Player> map = (Map<String, Player>) msg1.getData();
+                        if(msg1.getType() == Type.ShowAllPlayerInfo){
+                            // Second type cast (get all player's info as a Map)
+                            Map<String, Player> map = (Map<String, Player>) msg1.getData();
 
-                        // For-each loop to get every player's info in that map, then extract required info
-                        for(var player : map.entrySet()) {
-                            // This is how you access the data
-                            System.out.printf("%s, %s, %s%n", player.getValue().username, player.getValue().balance, player.getValue().isDealer);
+                            // For-each loop to get every player's info in that map, then extract required info
+                            for(var player : map.entrySet()) {
+                                // This is how you access the data
+                                System.out.printf("%s, %s, %s%n", player.getValue().username, player.getValue().balance, player.getValue().isDealer);
+                            }
                         }
                     } catch (ClassCastException e) {
                         System.out.println(e.getMessage());
