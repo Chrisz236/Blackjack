a7a5bf66d6bb2b3cc9d134a9f7761d1d1e5d3456
amanuel <eman@emans-air>
2022-07-27 16:00:12 -0700

    Updated class for  dealer and player , junit test

---
+++ BlackJack/PD/DealerTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DealerTest {
+
+	 @Test
+     public void ConstructorTest() {
+	Dealer dealer=new Dealer("amanuel");
+	
+	assertAll("check  constructor initilization", () -> assertNotNull(dealer.getHand()), () -> assertEquals("amanuel", dealer.getName()),
+			() -> assertEquals(0, dealer.handCounter));
+	 }
+
+	 @Test
+	 public void HandTest() {
+		 Dealer dealer=new Dealer("amanuel");
+		 dealer.setHand("A1");
+		 dealer.setHand("Q3"); 
+		 
+		 String hand[]=dealer.getHand();
+		 assertAll("check Default constructor initilization", () -> assertEquals("A1", hand[0]),
+					() -> assertEquals("Q3", hand[1]));
+			 }
+
+		 
+	 
+}
