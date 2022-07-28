dc2830c8d7b033c923462ff749856f24779d8e8e
amanuel <eman@emans-air>
2022-07-27 19:52:03 -0700

    test suit is done

--- BlackJack/PD/AllTests.java
+++ BlackJack/PD/AllTests.java
@@ -4,7 +4,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({DealerTest.class,PlayersTest.class,TestLobby.class,TestLobbyManager.class,TestPlayer.class,TestServer.class
-	, TestServerDriver.class, WindowTester.class})
+	, TestServerDriver.class, WindowTester.classTestClient.class,TestClientDriver.class})
 public class AllTests {
 
 }
